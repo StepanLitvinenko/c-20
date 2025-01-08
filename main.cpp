ce2c385d79c4043378e8a25752862ac88956015d
Литвиненко Степан Алексеевич <you@example.com>
2025-01-08 19:30:38 +0300

    демонстрация отсутствия говнища с enable if

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <concepts>
+template <typename T>
+int foo(T a) requires std::is_same<T, float>::value
+{
+    std::cerr << " float overloading " << std::endl;
+    return 0;
+}
+
+template <typename T>
+int foo(T a) requires (!std::is_same<T, float>::value)
+{
+    std::cerr << " other overloading " << std::endl;
+    return 0;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    float a;
+    double b;
+    int c;
+    float d;
+
+
+    foo(a);
+    foo(b);
+    foo(c);
+    foo(d);
+
     return 0;
 }
